c1eb058540f806009107e5d25d85189546dc2a7c
mayi12 <473407177@qq.com>
2020-02-09 20:59:29 +0800

    some changes3

--- src/main/java/com/weteam/entity/Message.java
+++ src/main/java/com/weteam/entity/Message.java
@@ -1,19 +1,24 @@
 package com.weteam.entity;
 
+import javax.persistence.*;
 import java.io.Serializable;
 import java.util.Date;
 
+@Entity
+@Table(name = "message")
 public class Message implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
-
+    @Column(name = "create_time")
     private Date createTime;
 
     private String title;
-
+    @Column(name = "img_url")
     private String imgUrl;
 
     private String content;
-
+    @Column(name = "user_id")
     private Integer userId;
 
     private static final long serialVersionUID = 1L;
